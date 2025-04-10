f7c067fb9f578052fd4c0d7d87c77634089a6058
avongs <avongspharuna@gmail.com>
2025-04-10 16:17:40 +0100

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
-  get "sessions/destroy"
-  get "users/new"
-  get "users/create"
-  get "users/show"
-  get "users/destroy"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "sessions#new"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:new, :create, :show, :destroy]
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get    "sign_in",  to: "sessions#new"
+  post   "sign_in",  to: "sessions#create"
+  delete "sign_out", to: "sessions#destroy"
 end
